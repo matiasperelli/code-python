c7c0589d29b3e7e8a29eb72689ba5af2ef2bae33
mperelli <101881175+matiasperelli@users.noreply.github.com>
2023-07-16 19:21:20 -0400

    Create Einstein.py

---
+++ Einstein.py
@@ -0,0 +1,7 @@
+m = int(input("Enter the mass in KG: ")) # The user enters the mass in kilograms
+
+c = 300000000*300000000 # The calculation of the speed of light
+
+E = m*c # Calculate energy
+
+print(f"E:", E) # Print the result of the Einstein equation
