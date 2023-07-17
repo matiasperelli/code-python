e1a1c1995c1b4b39ed20da22aca5984ccaf85b8a
mperelli <101881175+matiasperelli@users.noreply.github.com>
2023-07-17 18:02:37 -0400

    Create deep.py

---
+++ deep.py
@@ -0,0 +1,8 @@
+txt = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ") # User input
+
+if txt == "42" or txt == "Forty-two" or txt == "Forty two": # Case 1, the user types the number "42" in any of those options
+    print("Yes") # So, te program says yes
+else:
+    print("No") # If the user types something different, print "No"
+
+#Harvard cs50 #5
