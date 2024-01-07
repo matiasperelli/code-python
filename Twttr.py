4443e8b1eb52f172bebb6b419b4b783b21da4834
mperelli <101881175+matiasperelli@users.noreply.github.com>
2024-01-07 13:03:32 -0300

    Create Twttr.py

---
+++ Twttr.py
@@ -0,0 +1,13 @@
+# Get the user input
+txt = str(input("Input: "))
+
+# Create the output without vowels
+txt = txt.replace("a", "")
+txt = txt.replace("e", "")
+txt = txt.replace("i", "")
+txt = txt.replace("o", "")
+txt = txt.replace("u", "")
+
+print(txt)
+
+#Harvard CS50
